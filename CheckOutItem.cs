b3d7dbdedac6a6ada2e75b6db0b6840aa7098576
JW33R <JW33R@DESKTOP-6I0O4P4>
2025-11-25 23:37:46 -0500

    Added All Old Content

---
+++ CheckOutItem.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManager
+{
+    internal class CheckoutItem
+    {
+        public static List<LibraryItem> CheckoutItems = new();
+
+        public LibraryItem Item { get; set; }
+        public int DueDate { get; set; }
+
+        public CheckoutItem(LibraryItem item, int dueDate)
+        {
+            Item = item;
+            DueDate = dueDate;
+        }
+
+        /*
+        int dueDate = 3;
+        public void TakeItemOut()
+        {
+            Program.ClearScreen();
+            Console.WriteLine("Enter the ID of the item you wish to check out: ");
+            int tempID = Convert.ToInt32(Console.ReadLine());
+            var selectedItem = Program.Catalog.FirstOrDefault(item => item.ItemID == tempID && !item.IsCheckedOut);
+            if (selectedItem != null)
+            {
+                selectedItem.IsCheckedOut = true;
+                CheckoutItems.Add(selectedItem);
+                Console.WriteLine($"You have checked out: {selectedItem.Title}");
+                Console.WriteLine($"Due date is in {dueDate} days.");
+            }
+            else
+            {
+                Console.WriteLine("Item not found or already checked out.");
+            }
+            Program.PressContinue();
+        }*/
+    }
+}
---
+++ LibraryItem.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManager
+{
+    internal class LibraryItem
+    {
+        public int ItemID { get; set; }
+        public string Title { get; set; }
+        public string ItemType { get; set; }
+        public decimal DailyLateFee { get; set; }
+
+        public bool IsCheckedOut { get; set; }
+
+        public LibraryItem(int itemID, string title, string itemType, decimal dailyLateFee)
+        {
+            ItemID = itemID;
+            Title = title;
+            ItemType = itemType;
+            DailyLateFee = dailyLateFee;
+
+        }
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"ID: {ItemID}, Title: {Title}, Type: {ItemType}, Daily Late Fee: {DailyLateFee}");
+        }
+    }
+}
