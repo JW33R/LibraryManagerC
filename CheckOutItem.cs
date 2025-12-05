3e92e2b959694208c3f79410acbe15f63a024c28
Tyler <108450341+JW33R@users.noreply.github.com>
2025-12-04 20:39:39 -0500

    Improve formatting and comments in CheckoutItem
    Refactor CheckoutFormat and LateFee methods for clarity.

--- CheckOutItem.cs
+++ CheckOutItem.cs
@@ -17,7 +17,8 @@ namespace LibraryManager
         }
         //I used AI to help me figure out a better way to format the checkout receipt. I asked it: What are some methods to format in c#?
         //It suggested a few other methods along with PadRight which I decided to use because it was easy to understand and implement.
-        public static void CheckoutFormat()
+        public static void CheckoutFormat()//This function does the formating for the reciept I use pad right and have it loop thorugh each item in the checkoutItems list and have it print out the ID, Title, Type, DaysLate, and Late Fee
+        //Along with the total amount they will owe
         {
             Console.WriteLine("------------- Checkout Receipt ------------");
             Console.WriteLine("ID".PadRight(7) + "Title".PadRight(25) + "Type".PadRight(15) + "Days Late".PadRight(13) + "Fee");
@@ -29,7 +30,8 @@ namespace LibraryManager
             Console.WriteLine("-------------------------------------------");
             totalLateFees = 0;
         }
-        public static void LateFee(int day, LibraryItem item, CheckoutItem checkout)
+        public static void LateFee(int day, LibraryItem item, CheckoutItem checkout)//This function determines how much the user will have to owe in late fees. It checks to see if the days parameter is larger than the DueDate, and if it is
+        //Then it takes the day entered and subtracts the DueDate and puts that into DaysLate then takes the DailyLateFee and multiplies it by the amount of DaysLate and puts that into ItemLateFee. It then adds that late fee to totalLateFees
         {
             if (day > checkout.DueDate)
             {
